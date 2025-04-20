4c5bb80bbecc18c2b763107062f8469acccc7551
devasherr <ashersam116@gmail.com>
2025-04-20 11:46:42 +0300

    pass add and absent value through flags

--- cmd/root.go
+++ cmd/root.go
@@ -8,9 +8,15 @@ var (
 		Short: "Absent tracker",
 		Long:  `Skip classes with freedom`,
 	}
+
+	defaultAbsentCount int
+	absentCount        int
 )
 
 func init() {
+	addCmd.PersistentFlags().IntVar(&defaultAbsentCount, "absent", 1, "default absent out for course")
+	absentCmd.PersistentFlags().IntVar(&absentCount, "count", 1, "absent count")
+
 	rootCmd.AddCommand(resetCmd)
 	rootCmd.AddCommand(absentCmd)
 	rootCmd.AddCommand(addCmd)
